4ce1dcd799efb275c6db9d62a007bafbe2f332f3
IvanMathyn <winter.nes@gmail.com>
2022-01-26 14:24:45 +0200

    Add some feture

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
+  StyleSheet, Text, TextInput,
   useColorScheme,
   View
 } from 'react-native';
@@ -14,8 +14,17 @@ import Crashes from 'appcenter-crashes'
 import {
   Colors
 } from 'react-native/Libraries/NewAppScreen';
+import * as Analytics from 'appcenter-analytics';
 
 const App: () => Node = () => {
+  const [amount, setAmount] = useState();
+  const [inflationRate, setInflationRate] = useState();
+  const [timeInYears, setTimeInYears] = useState();
+  const [riskFreeRate, setRiskFreeRate] = useState();
+  const [afterInflation, setAfterInflation] = useState();
+  const [atRiskFree, setAtRiskFree] = useState();
+  const [atRiskFreeAfterInflation, setAtRiskFreeAfterInflation] = useState();
+  const [difference, setDifference] = useState();
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -24,16 +33,35 @@ const App: () => Node = () => {
 
   const throwError = async () => {
     const data = await Crashes.generateTestCrash();
-    console.log(27, 'zxc' ,'data', data)
+    console.log(27, 'zxc', 'data', data)
   }
 
   const checkIsCrashed = async () => {
     const isCrashed = await Crashes.hasCrashedInLastSession();
-    console.log(31, 'zxc' ,'isCrashed', isCrashed)
+    console.log(31, 'zxc', 'isCrashed', isCrashed)
     if (isCrashed) {
       const report = await Crashes.lastSessionCrashReport();
-      console.log(34, 'zxc' ,'report', report)
+      console.log(34, 'zxc', 'report', report)
     }
+  }
+
+  const calculateInflationImpact = (value, inflationRate, time) => {
+    return value / Math.pow(1 + inflationRate, time);
+  }
+
+  const calculate = async () => {
+    const afterInflationVal = calculateInflationImpact(amount, inflationRate / 100, timeInYears);
+    const atRiskFreeVal = amount * Math.pow(1 + riskFreeRate / 100, timeInYears);
+    const atRiskFreeAfterInflationVal = calculateInflationImpact(atRiskFree, inflationRate / 100, timeInYears);
+    const differenceVal = Math.round(atRiskFreeAfterInflation - afterInflation);
+
+    setAfterInflation(Math.round(afterInflationVal))
+    setAtRiskFree(Math.round(atRiskFreeVal))
+    setAtRiskFreeAfterInflation(Math.round(atRiskFreeAfterInflationVal))
+    setAfterInflation(Math.round(afterInflationVal))
+    setDifference(Math.round(differenceVal))
+
+    await Analytics.trackEvent('calculate_inflation', { Internet: 'WiFi', GPS: 'Off' });
   }
 
   return (
@@ -46,6 +74,28 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white
           }}>
+          <View style={styles.sectionContainer}>
+            <TextInput placeholder="Current inflation rate"
+                       style={styles.textBox} keyboardType="decimal-pad"
+                       onChangeText={setInflationRate}/>
+            <TextInput placeholder="Current risk free rate"
+                       style={styles.textBox} keyboardType="decimal-pad"
+                       onChangeText={setRiskFreeRate}/>
+            <TextInput placeholder="Amount you want to save"
+                       style={styles.textBox} keyboardType="decimal-pad"
+                       onChangeText={setAmount}/>
+            <TextInput placeholder="For how long (in years) will you save?"
+                       style={styles.textBox} keyboardType="decimal-pad"
+                       onChangeText={setTimeInYears}/>
+            <Button title="Calculate inflation"
+                    onPress={calculate}
+            />
+            <Text style={styles.label}>{timeInYears} years from now you will still have ${amount} but it will only be
+              worth ${afterInflation}.</Text>
+            <Text style={styles.label}>But if you invest it at a risk free rate you will have ${atRiskFree}.</Text>
+            <Text style={styles.label}>Which will be worth ${atRiskFreeAfterInflation} after inflation.</Text>
+            <Text style={styles.label}>A difference of: ${difference}.</Text>
+          </View>
           <Button title={'Crash'} onPress={throwError}/>
           <Button title={'Check'} onPress={checkIsCrashed}/>
         </View>
@@ -70,7 +120,16 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700'
-  }
+  },
+  label: {
+    marginTop: 10
+  },
+  textBox: {
+    height: 30,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10
+  },
 });
 
 export default App;
